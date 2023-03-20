c31ced50407f130362225a65356063eaee63692e
Olga-Dames <113420629+Olga-Dames@users.noreply.github.com>
2023-03-20 23:01:33 +0200

    123

--- src/modules/news-box/news-box.js
+++ src/modules/news-box/news-box.js
@@ -1,11 +1,8 @@
-import moment from 'moment';
-import ApiArticlesService from '../service/articles';
+import articlesService from '../service/articles';
 import articleTpl from './templates/article.hbs';
+import notifier from '../service/notifier';
 
-/* 
-    Написати додаток пошуку новин
-*/
-const apiArticleService = new ApiArticlesService();
+// Написати додаток пошуку новин
 
 class NewsBox {
   #markup = `
@@ -32,114 +29,120 @@ class NewsBox {
     <div class="articles"></article> 
     </div>
     <section class="load-more">
-    <button class="load-more__btn" hidden>More</button>
+    <button class="load-more__btn load-more__btn_hidden">More</button>
     </section>
     </section>
     `;
-  #targetElement = null;
-  #refs = {};
-  #articles = [];
-  #articleService = () => {};
 
-  constructor({ targetElement, articleService } = {}) {
+  #targetElement = null;
+
+  #searchQuery = null;
+
+  #refs = {};
+
+  #articles = [];
+
+  #infiniteLoading = false;
+
+  constructor({ targetElement, infiniteLoading = false } = {}) {
     this.#targetElement = targetElement || document.body;
-    this.#articleService = articleService;
+    this.#infiniteLoading = infiniteLoading;
   }
 
   init() {
-    if (this.#articleService) {
-      this.#targetElement.innerHTML = this.#markup;
-      this.#initRefs();
-      this.#initListeners();
-    } else {
-      console.log('"articleService" is require');
+    this.#targetElement.innerHTML = this.#markup;
+    this.#initRefs();
+    this.#initListeners();
+
+    if (this.#infiniteLoading) {
+      this.#initInfiniteLoading();
     }
   }
 
   #initRefs() {
     this.#refs.search = document.querySelector('.news-app .search');
     this.#refs.articles = document.querySelector('.news-app .articles');
+    this.#refs.moreBtn = document.querySelector('.news-app .load-more__btn');
   }
 
   #initListeners() {
     this.#refs.search.addEventListener('submit', this.#onSearch.bind(this));
+    this.#refs.moreBtn.addEventListener('click', this.#onClickLoadMoreBtn.bind(this));
   }
 
   #updateArticles(articles) {
     this.#articles = articles;
     this.#render();
+    if (!this.#infiniteLoading) {
+      this.#toggleMoreBtn();
+    }
   }
 
   #onSearch(e) {
     e.preventDefault();
-    const searchQuery = e.currentTarget.elements.search.value;
-
-    this.#articleService
-      .fetchData(searchQuery)
+    this.#searchQuery = e.currentTarget.elements.search.value;
+    this.#fetchArticles()
       .then((articles) => {
+        if (articles.length === 0) {
+          return notifier.info('No results. Please clarify your search');
+        }
         this.#updateArticles(articles);
-      })
-      .catch((error) => {
-        console.log(error);
       })
       .finally(() => {
         e.target.reset();
       });
   }
 
-  #render() {
-    const mockup = this.#articles
-      .map(({ author, title, url, urlToImage, publishedAt, content }) => {
-        const { text, timeForReading } = parseContent(content);
+  #loadMore() {
+    return this.#fetchArticles().then((articles) => {
+      this.#updateArticles([...this.#articles, ...articles]);
+    });
+  }
 
-        return articleTpl({
-          author,
-          title,
-          url,
-          content,
-          text,
-          urlToImage,
-          timeForReading,
-          publishDate: moment(publishedAt).format('lll'),
-        });
-      })
-      .join('');
+  #fetchArticles() {
+    return articlesService
+      .fetchData(this.#searchQuery)
+      .then((articles) => articles)
+      .catch((error) => {
+        console.log(error);
+        notifier.error('Something went wrong. Please try later');
+      });
+  }
+
+  #onClickLoadMoreBtn() {
+    this.#refs.moreBtn.classList.add('load-more__btn_loading');
+    this.#refs.moreBtn.disabled = true;
+    this.#loadMore().finally(() => {
+      this.#refs.moreBtn.classList.remove('load-more__btn_loading');
+      this.#refs.moreBtn.disabled = false;
+    });
+  }
+
+  #toggleMoreBtn() {
+    if (this.#articles.length > 0) {
+      this.#refs.moreBtn.classList.remove('load-more__btn_hidden');
+    } else {
+      this.#refs.moreBtn.classList.add('load-more__btn_hidden');
+    }
+  }
+
+  #initInfiniteLoading() {
+    const observer = new IntersectionObserver((entries) => {
+      for (const entry of entries) {
+        if (entry.isIntersecting && this.#articles.length > 0) {
+          this.#loadMore();
+        }
+      }
+    }, { rootMargin: '400px' });
+    observer.observe(this.#refs.moreBtn);
+  }
+
+  #render() {
+    const mockup = this.#articles.map((data) => articleTpl({ ...data })).join('');
 
     this.#refs.articles.innerHTML = mockup;
   }
 }
 
-const newsBox = new NewsBox({ articleService: new ApiArticlesService() });
+const newsBox = new NewsBox({ infiniteLoading: true });
 newsBox.init();
-
-function parseContent(content) {
-  const firstSeparatorPosition = content.indexOf('[');
-  const secondSeparatorPosition = content.indexOf(']');
-  const parseResult = {
-    text: null,
-    timeForReading: null,
-  };
-
-  if (firstSeparatorPosition >= 0 && secondSeparatorPosition >= 0) {
-    const numberOfChars = parseInt(content.substring(firstSeparatorPosition + 1, secondSeparatorPosition));
-
-    parseResult.text = content.substring(0, firstSeparatorPosition);
-    parseResult.timeForReading = calculateTimeForReading(numberOfChars);
-  }
-
-  return parseResult;
-}
-
-function calculateTimeForReading(numberOfChars) {
-  const minutes = Math.floor(numberOfChars / 255);
-
-  if (minutes === 0) {
-    return 'less then one minute';
-  }
-
-  if (minutes > 0 && minutes < 2) {
-    return 'more then one minute';
-  }
-
-  return `${minutes} minutes`;
-}
